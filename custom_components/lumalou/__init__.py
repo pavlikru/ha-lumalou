07047de20dcefa6c53da2c89311671de8568c0f5
Pavel Maltsev <pavel.maltsev@improvado.io>
2026-09-21 18:05:39 -0300

    fix: satisfy public validation workflow

--- custom_components/lumalou/__init__.py
+++ custom_components/lumalou/__init__.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers import issue_registry as ir
 from homeassistant.helpers.typing import ConfigType
 
@@ -12,6 +13,8 @@ from .models import LumalouRuntimeData
 from .services import async_setup_services
 
 type LumalouConfigEntry = ConfigEntry[LumalouRuntimeData]
+
+CONFIG_SCHEMA = cv.config_entry_only_config_schema(DOMAIN)
 
 
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
